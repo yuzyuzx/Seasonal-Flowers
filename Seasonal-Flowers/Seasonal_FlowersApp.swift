4f93f3343893f5ed3d386c1c6ca89ec4858825d8
yuz <yuzyuzx@gmail.com>
2024-05-13 12:53:47 +0900

    Initial Commit

---
+++ Seasonal-Flowers/Seasonal_FlowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Seasonal_FlowersApp.swift
+//  Seasonal-Flowers
+//
+//  Created by Yuz on 2024/05/13.
+//
+
+import SwiftUI
+
+@main
+struct Seasonal_FlowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
